31f7cbef2e864d4fd4e96e8ec22564eef3ebaefe
ArmanPipoyan <64909828+ArmanPipoyan@users.noreply.github.com>
2020-12-13 14:50:12 +0100

    Added 5 automated games

--- src/test/java/Othello/AutomatedTestGames.java
+++ src/test/java/Othello/AutomatedTestGames.java
@@ -7,10 +7,35 @@ public class AutomatedTestGames {
 	@Test
 	//Uses keyboard input mock and realize Path Coverage
 	public void integersCoordinatesTest() {	
-		Othello othelloWinBlacks = new Othello();
-		KeyboardInput input1 = new AutomatedTestInputs();
-		othelloWinBlacks.setInput(input1);
-		othelloWinBlacks.play();
-	}
+		Othello whitePassBug = new Othello();
+		KeyboardInput input1 = new AutomatedTestInputs(1);
+		whitePassBug.setInput(input1);
+		whitePassBug.play();
 
+		Othello drawGame = new Othello();
+		KeyboardInput input2 = new AutomatedTestInputs(2);
+		drawGame.setInput(input2);
+		drawGame.play();
+		
+
+		Othello notFullBoard = new Othello();
+		KeyboardInput input3 = new AutomatedTestInputs(3);
+		notFullBoard.setInput(input3);
+		notFullBoard.play();
+		
+		Othello wipeOut = new Othello();
+		KeyboardInput input4 = new AutomatedTestInputs(4);
+		wipeOut.setInput(input4);
+		wipeOut.play();
+		
+		Othello whiteWins = new Othello();
+		KeyboardInput input5 = new AutomatedTestInputs(5);
+		whiteWins.setInput(input5);
+		whiteWins.play();
+
+		Othello blackWins = new Othello();
+		KeyboardInput input6 = new AutomatedTestInputs(6);
+		blackWins.setInput(input6);
+		blackWins.play();
+	}
 }
