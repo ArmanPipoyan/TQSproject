9b476d14372f399f55c67300b93159aaa7403584
Alejandro <AlejandroPerezma@users.noreply.github.com>
2020-12-15 13:02:36 +0100

    Automated tests finished

--- src/test/java/Othello/AutomatedTestGames.java
+++ src/test/java/Othello/AutomatedTestGames.java
@@ -3,46 +3,61 @@ package Othello;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class AutomatedTestGames {
+import org.junit.Ignore;
 
-	@Test
-	//Uses keyboard input mock and realize Path Coverage
-	public void integersCoordinatesTest() {	
+public class AutomatedTestGames {
+	
+	@Test(timeout=1000)
+	public void whitePassBugTest() {	
 		Othello whitePassBug = new Othello();
 		KeyboardInput input1 = new AutomatedTestInputs(1);
 		whitePassBug.setInput(input1);
 		whitePassBug.play();
 		assertTrue(whitePassBug.getGame().getGameBoard().isFull());
-
+	}
+	
+	@Test()
+	public void drawGameTest() {	
 		Othello drawGame = new Othello();
 		KeyboardInput input2 = new AutomatedTestInputs(2);
 		drawGame.setInput(input2);
 		drawGame.play();
-		assertTrue(whitePassBug.getGame().getGameBoard().isFull());
-		
-
+		assertTrue(drawGame.getGame().getGameBoard().isFull());
+	}
+	
+	@Test(timeout=1000)
+	public void notFullBoardTest() {
 		Othello notFullBoard = new Othello();
 		KeyboardInput input3 = new AutomatedTestInputs(3);
 		notFullBoard.setInput(input3);
 		notFullBoard.play();
-		assertTrue(whitePassBug.getGame().getGameBoard().isFull());
-		
+		assertTrue(notFullBoard.getGame().getGameBoard().isFull());
+	}
+	
+	@Test(timeout=1000)
+	public void wipeOutTest() {
 		Othello wipeOut = new Othello();
 		KeyboardInput input4 = new AutomatedTestInputs(4);
 		wipeOut.setInput(input4);
 		wipeOut.play();
-		assertTrue(whitePassBug.getGame().getGameBoard().isFull());
-		
+		assertTrue(wipeOut.getGame().getGameBoard().isFull());
+	}
+	
+	@Test()
+	public void whiteWinsTest() {
 		Othello whiteWins = new Othello();
 		KeyboardInput input5 = new AutomatedTestInputs(5);
 		whiteWins.setInput(input5);
 		whiteWins.play();
-		assertTrue(whitePassBug.getGame().getGameBoard().isFull());
-
+		assertTrue(whiteWins.getGame().getGameBoard().isFull());
+	}
+	
+	@Test()
+	public void blackWinsTest() {
 		Othello blackWins = new Othello();
 		KeyboardInput input6 = new AutomatedTestInputs(6);
 		blackWins.setInput(input6);
 		blackWins.play();
-		assertTrue(whitePassBug.getGame().getGameBoard().isFull());
+		assertTrue(blackWins.getGame().getGameBoard().isFull());
 	}
 }
